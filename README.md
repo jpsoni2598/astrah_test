eee044ff01855024eb90c2b88ff17ff27962bc1f
Jayesh Pamnani <jpsoni2598@gmail.com>
2025-11-24 22:41:17 +0530

    feat: initial module structure setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Astrah Test Module
+
+Odoo 19 custom module created as part of the Astrah OS technical evaluation.
---
+++ __manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': "Astrah Test",
+    'version': '1.0',
+    'depends': [],
+    'author': "Jayesh Pamnani",
+    'category': 'CRM',
+    'description': """
+        Test task module for Astrah OS evaluation.
+        Demonstrates ORM, custom views, Tailwind integration,
+        and an OWL component in Odoo 19.
+    """,
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menus.xml',
+        'views/lead_form.xml',
+    ],
+
+    'demo': [
+    ],
+
+    'assets': {
+        'web.assets_backend': [
+            'astrah_test/static/src/css/tailwind.css',
+            'astrah_test/static/src/js/test_component.js',
+            'astrah_test/static/src/xml/test_component.xml',
+        ],
+    },
+
+    'installable': True,
+    'application': False,
+}
